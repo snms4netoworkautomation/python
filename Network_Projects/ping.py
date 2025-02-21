385d21a8ab76fe568ce23c7626bc566a5af61fb7
Thilagaraj Rajasekar <rajatx@arise.com>
2025-02-22 00:46:59 +0530

    deleted:    Extract.py 	deleted:    Git_Tips.txt 	deleted:    Network_Projects/Python Program to Generate a Multiplication Table.py 	deleted:    Network_Projects/Test-delete.py 	deleted:    Network_Projects/What is my IP Verion1.py 	deleted:    Network_Projects/What is my IP copy.py 	deleted:    Network_Projects/What is my IP.py 	deleted:    Network_Projects/extractor_for_txt.py 	deleted:    Network_Projects/ip.py 	deleted:    Network_Projects/ping.py 	deleted:    data.csv 	deleted:    example.txt 	deleted:    ip.txt 	deleted:    python 	deleted:    python copy 	deleted:    python.code-workspace 	deleted:    python_tips_small_program/Python_tips 	deleted:    python_tips_small_program/WHEN_eval and int  copy.py 	deleted:    python_tips_small_program/WHEN_eval and int .py 	deleted:    python_tips_small_program/just.py 	deleted:    python_tips_small_program/whilevsfor.py 	deleted:    students.txt

--- Network_Projects/ping.py
+++
@@ -1,8 +0,0 @@
-from ping3 import ping
-
-iplist = ["youtube.com","google.com","4.4.4.4"]
-
-for i in iplist:
-    respone = ping(i,timeout=2)
-    print(i,respone*1000)
-    
--- python_tips_small_program/just.py
+++
@@ -1,9 +0,0 @@
-numbers = [1, 2, 3, 4, 5]
-i = len(numbers) - 1
-print(i)
-print(numbers[i])
-while i >= 0:
- print(numbers[i], end=' ')
- i -= 1
- print(i,"asdasda")
-
