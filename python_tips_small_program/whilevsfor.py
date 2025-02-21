967246944ee821bf95d5298c5086e3d47dab9d64
Thilagaraj Rajasekar <rajatx@arise.com>
2025-02-22 00:52:18 +0530

    new file:   python

---
+++ python_tips_small_program/whilevsfor.py
@@ -0,0 +1,38 @@
+
+
+for i in range(100,49,-1):
+    print(i)    
+for i in range(5,14,2):
+    print(i)
+n = 5
+while (n>0):
+    print("Thilagaraj_R Architect")
+    n-=1
+n = 0
+while(n<5):
+    print("THILAGARAJ_R ARCHITECT")
+    n=n+1
+    
+my_list = list(range(1, 7)) 
+for index, item in enumerate(my_list): 
+ my_list.pop(index)
+print(my_list)    
+    
+while True:
+    User_input =input("Enter your name: ")
+    #Break the loop if the user ends the END
+    User_input = User_input.upper()
+    print(User_input)
+    if User_input == "END":
+        print("the loop is ended")
+        break
+    print(f"you name is {User_input}")
+
+
+import re
+
+#The search() function returns a Match object:
+
+txt = "The rain in Spain"
+x = re.search()
+print(x)
